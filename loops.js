e3f6d566655afa9a0da1bf9e76411086672d69a5
singhrahulkanwar@gmail.com Singh <singhrahulkanwar@gmail.com>
2018-04-06 07:31:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,15 +4,15 @@ function wrapGift(gift) {
   console.log(`Wrapped ${gift} and added a bow!`)
 }
 
-function maybeTrue() 
-{
-  return Math.random() >= 0.5
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
 
-do
-{
-  console.log('doo-bee-doo-bee-doo')
-} while (maybeTrue());
+// run until `maybeTrue()` returns `false`
+// (so the body of the loop might _never_ run!)
+while (maybeTrue()) {
+  console.log("And I ran; I ran so far away!");
+}
 
 function doWhileLoop(gifts)
 {
