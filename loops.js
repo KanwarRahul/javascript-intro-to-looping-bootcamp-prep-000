1568d928b1bd8e176bf9492caac05fc7f4661203
singhrahulkanwar@gmail.com Singh <singhrahulkanwar@gmail.com>
2018-04-06 07:19:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,7 +4,15 @@ function wrapGift(gift) {
   console.log(`Wrapped ${gift} and added a bow!`)
 }
 
+function maybeTrue() 
+{
+  return Math.random() >= 0.5
+}
 
+do {
+
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 
 function doWhileLoop()
 {
